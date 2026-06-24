8ae527bc939e005a8b22940776d8cc05d1e3a803
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-06-24 11:20:23 +0430

    add picture to the list display of product in admin pannel

--- mezon/admin.py
+++ mezon/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Category, Color, Size, Product, ProductPicture
 
@@ -26,6 +27,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = [
         'id',
         'title',
+        'product_picture',
         'category',
         'inventory',
         'price',
@@ -44,4 +46,14 @@ class ProductAdmin(admin.ModelAdmin):
     ]
     list_per_page = 10
     inlines = [ProductPictureInLine,]
+
+    def product_picture(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="60" height="60" style="border-radius:8px;" />',
+                obj.image.url
+            )
+        return "-"
+    
+    product_picture.short_description = "Image"
     
