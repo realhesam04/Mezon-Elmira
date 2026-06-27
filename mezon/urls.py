bc455f14a45170b112dea00c69c570957e9792e5
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-06-27 14:35:53 +0430

    add about-us url

--- mezon/urls.py
+++ mezon/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('about-us/', views.about_us, name='about_us',),
 
     path('products/',views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
