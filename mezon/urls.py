5ee7455df599f4a80026fba3ea5b907d2d595f4a
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-07-01 18:20:22 +0430

    add contact_us url

--- mezon/urls.py
+++ mezon/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('about-us/', views.about_us, name='about_us',),
+    path('contact-us/', views.contact_us, name='contact_us',),
 
     path('products/',views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
