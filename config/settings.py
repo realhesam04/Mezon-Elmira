edd51c1756583347ee6f02c5671c30bdf6d0bd2f
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-07-12 14:47:09 +0430

    add static root and changed some lines in settings.py

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
