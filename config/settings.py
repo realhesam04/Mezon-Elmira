e61ba898f7bd9633db6ff496dac08a4425a1de39
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-08-03 02:03:59 +0430

    change some issues on settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,9 +134,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE =  (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage",
-)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
