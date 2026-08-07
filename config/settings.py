eb7384ea2be29218efda67d6446bf7ee67bd34a4
realhesam <hesamehsanimoghaddam2004@gmail.com>
2026-08-07 06:32:06 +0430

    trying to solve databse problem

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,8 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -95,10 +97,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+    )
 }
 
 
